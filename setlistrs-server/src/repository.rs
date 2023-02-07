d509e8a0f65b3d5690139f5a34200d7a73bb7f30
Wojciech Gula <gulasz101@gmail.com>
2023-02-07 01:12:51 +0100

    WIP

--- setlistrs-server/src/repository.rs
+++ setlistrs-server/src/repository.rs
@@ -37,41 +37,78 @@ ORDER BY id
     })
     .collect();
 
-    songs.into_iter().for_each(async |(song_id, mut song)| {
-        let cover = query!(
-            r#"
+    let song_ids = songs.iter().map(|(song_id, _song)| song_id);
+    let mut covers: Vec<(i64, YTLink)> = Vec::new();
+    for song_id in song_ids.clone() {
+        covers.push((song_id.clone(), obtain_cover(pool, song_id).await?));
+    }
+
+    let mut sources: Vec<(i64, Vec<YTLink>)> = Vec::new();
+    for song_id in song_ids {
+        sources.push((song_id.clone(), obtain_sources(pool, song_id).await?));
+    }
+
+    // let test: Vec<Vec<YTLink>> = sources
+    //     .into_iter()
+    //     .map(|(song_id, cover)| cover.clone())
+    //     .collect();
+
+    songs.iter().map(|(song_id, song)| {
+        let song_sources: Vec<Vec<YTLink>> = sources
+            .iter()
+            .filter_map(|(source_song_id, yt_link)| match source_song_id {
+                song_id => Some(yt_link),
+                _ => None,
+            })
+            .cloned()
+            .collect();
+
+        song
+    });
+
+    Ok(songs)
+}
+
+async fn obtain_cover(pool: &SqlitePool, song_id: &i64) -> Result<YTLink> {
+    Ok(query!(
+        r#"
 SELECT l.url, l.display_title FROM covers c, links l 
 WHERE c.song_id = ? 
 AND l.id = c.link_id
 ORDER BY c.id
             "#,
-            song_id
-        )
-        .map(|link| YTLink {
-            url: match link.url {
-                Some(url) => url.into(),
-                None => "".into(),
-            },
-            display_title: link.display_title,
-        })
-        .fetch_one(pool)
-        .await
-        .unwrap();
-        song.cover = Some(vec![cover]);
-    });
-    //     let song_ids = songs.iter().map(|(song_id, _song)| song_id);
-    //     query!(
-    //         r#"
-    // SELECT *
-    // FROM sources
-    // WHERE song_id = ANY($1)
-    //         "#,
-    //         &song_ids
-    //     )
-    //     .fetch_all(pool)
-    //     .await?;
+        song_id
+    )
+    .map(|link| YTLink {
+        url: match link.url {
+            Some(url) => url.into(),
+            None => "".into(),
+        },
+        display_title: link.display_title,
+    })
+    .fetch_one(pool)
+    .await?)
+}
 
-    Ok(songs)
+async fn obtain_sources(pool: &SqlitePool, song_id: &i64) -> Result<Vec<YTLink>> {
+    Ok(query!(
+        r#"
+SELECT l.url, l.display_title FROM sources s, links l 
+WHERE s.song_id = ? 
+AND l.id = s.link_id
+ORDER BY s.id
+            "#,
+        song_id
+    )
+    .map(|link| YTLink {
+        url: match link.url {
+            Some(url) => url.into(),
+            None => "".into(),
+        },
+        display_title: link.display_title,
+    })
+    .fetch_all(pool)
+    .await?)
 }
 
 pub async fn persist_song(pool: &SqlitePool, song: Song) -> Result<Song> {
