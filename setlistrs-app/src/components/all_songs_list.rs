2ffdb437b1a4dca3ddbe9e548a3f1a1d54297cc5
Wojciech Gula <gulasz101@gmail.com>
2023-02-15 21:54:27 +0100

    Moving component

--- setlistrs-app/src/components/all_songs_list.rs
+++ setlistrs-app/src/components/all_songs_list.rs
@@ -1,11 +1,13 @@
 use gloo_net::http::Request;
 use setlistrs_types::NewSetlist;
 use setlistrs_types::SongList;
-use web_sys::{FormData, HtmlInputElement};
+use web_sys::FormData;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
-use yew_router::prelude::use_navigator;
 
 use crate::app::Route;
+use crate::components::SongChockboxAction;
+use crate::components::SongsForSetlist;
 
 #[function_component(AllSongsList)]
 pub fn all_songs_list() -> Html {
@@ -37,36 +39,6 @@ pub fn all_songs_list() -> Html {
         );
     }
 
-    enum SongChockboxAction {
-        Add(i64),
-        Remove(i64),
-    }
-
-    struct SongsForSetlist {
-        pub data: Vec<i64>,
-    }
-
-    impl Reducible for SongsForSetlist {
-        type Action = SongChockboxAction;
-
-        fn reduce(self: std::rc::Rc<Self>, action: Self::Action) -> std::rc::Rc<Self> {
-            match action {
-                SongChockboxAction::Add(value) => {
-                    let mut list = self.data.clone();
-
-                    list.push(value);
-
-                    SongsForSetlist { data: list }.into()
-                }
-                SongChockboxAction::Remove(value) => {
-                    let mut list = self.data.clone();
-                    list.retain(|song_id| *song_id != value);
-                    SongsForSetlist { data: list }.into()
-                }
-            }
-        }
-    }
-
     let songs_for_setlist = use_reducer(|| SongsForSetlist { data: Vec::new() });
 
     let on_song_checkbox_click = {
@@ -84,58 +56,14 @@ pub fn all_songs_list() -> Html {
             }
         })
     };
-    let navigator = use_navigator().expect("There is no reason it will not work.");
-
-    let on_setlist_submit = {
-        let songs_for_setlist = songs_for_setlist.clone();
-
-        Callback::from(move |e: SubmitEvent| {
-            let navigator = navigator.clone();
-            e.prevent_default();
-            let form_data = FormData::new_with_form(&e.target_unchecked_into())
-                .expect("This is going to work since we are sure form exists.");
-
-            let new_setlist = NewSetlist {
-                display_title: match form_data.get("display_title").as_string() {
-                    Some(display_title) => display_title,
-                    None => panic!(),
-                },
-                songs: (*songs_for_setlist.data).to_vec(),
-            };
-
-            wasm_bindgen_futures::spawn_local(async move {
-                let response = Request::post("http://127.0.0.1:8081/setlists")
-                    .json(&new_setlist)
-                    .expect("This will work")
-                    .send()
-                    .await;
-
-                match response {
-                    Ok(possible_setlist_id) => {
-                        let setlist_id: Result<i64, _> = possible_setlist_id.json().await;
-                        match setlist_id {
-                            Ok(id) => navigator.push(&Route::Setlist { id }),
-                            Err(_) => panic!(),
-                        }
-                    }
-                    Err(_) => panic!(),
-                };
-            });
-        })
-    };
 
     html! {
     <>
         <article>
-            <form onsubmit={ on_setlist_submit }>
-                <fieldset disabled={ songs_for_setlist.data.is_empty() }>
-                    <label for="display_title">{"Setlist name"}</label>
-                    <input type="text" name="display_title" id="display_title"/>
-                    <button type="submit">
-                        {"Create setlist from checked songs"}
-                    </button>
-                </fieldset>
-            </form>
+            <crate::components::SetlistAdd
+                songs={(*songs_for_setlist).data.clone()}
+                disabled={&(*songs_for_setlist).data.is_empty()}
+            />
         </article>
     <table>
         <caption>{ "Setlist" }</caption>
--- setlistrs-app/src/components/mod.rs
+++ setlistrs-app/src/components/mod.rs
@@ -1,7 +1,9 @@
 mod add_song_form;
 mod all_songs_list;
+mod setlist_add;
 mod setlist_details;
 
 pub use add_song_form::*;
 pub use all_songs_list::*;
+pub use setlist_add::*;
 pub use setlist_details::*;
